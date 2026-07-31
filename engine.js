6fa49e42b065fbbd7a9e1831a2154633137aa468
CodeByPeace <starstorm1688@gmail.com>
2026-07-31 00:28:33 +0000

    Fix detailsOpened counter, add shuffle helper for replay value (#5 groundwork)

--- engine.js
+++ engine.js
@@ -110,6 +110,15 @@ function renderGroupChat(title, body) {
     ).join('');
 }
 
+function shuffleArray(arr) {
+    const a = arr.slice();
+    for (let i = a.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [a[i], a[j]] = [a[j], a[i]];
+    }
+    return a;
+}
+
 function renderGallery(title, body) {
     title.innerText = 'EVIDENCE_DUMP';
     const items = window.CASE.gallery || [];

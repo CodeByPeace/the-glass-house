021d394825f6268237ff5061e4178c046b126aef
CodeByPeace <starstorm1688@gmail.com>
2026-08-01 00:21:27 +0000

    Add assets.js manifest for real photo/audio URLs

---
+++ assets.js
@@ -0,0 +1,16 @@
+window.ASSETS = {
+    sarah_dock: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/sarah_01_dock.opus',
+    sarah_backroad: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/sarah_02_backroad.opus',
+    sarah_forensic: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/sarah_03_forensic.opus',
+    ashley_door: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/ashley_01_door.opus',
+    ashley_car: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/ashley_02_car.opus',
+    ashley_counter: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/ashley_03_counter.opus',
+    ashley_missing: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/ashley_04_missing.opus',
+    ashley_heading_over: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/ashley_05_heading_over.opus',
+    mirror_photo: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/photo_01_mirror.jpg',
+    dock_photo: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/photo_02_dock.jpg',
+    ticket_photo: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/photo_03_ticket.jpg',
+    coffee_photo: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/photo_04_coffee.jpg',
+    spotify_photo: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/photo_05_spotify.jpg',
+    rain_photo: 'https://raw.githubusercontent.com/CodeByPeace/the-glass-house/main/assets/photo_06_rain.jpg'
+};
